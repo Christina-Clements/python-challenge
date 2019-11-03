a0c986c6509db4fbaa62e5518b79e458ac2b9842
Christina Clements <crfc.tk@gmail.com>
2019-11-02 21:29:22 -0500

    PyPoll Part2

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -15,6 +15,34 @@ print(f'Election Results')
 print(f'----------------')    
 print(f'Total Votes = {row_count} ')
 
+with open(PyPoll_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+
+    candidate_name= []
+    for row in csvreader:
+        candidate_name.append(row[2])
+    used = []
+    for candidate in candidate_name:
+        if candidate in used:
+            pass
+        else:
+            used.append(candidate)
+    candidate_name= used
+    print(candidate_name)
+    
+    for candidate in candidate_name:
+        Total_Votes=0
+        for row in data:
+            if row[2] == candidate:
+                Total_Votes += 1
+        print(f'{candidate} : {Total_Votes}')
+            
+
+
+
+
+
 
     
 
